94220a5a761e985f9bb71ca20aa66982f01bc460
Willem Wyndham <wyndham@cs.unc.edu>
2023-05-31 13:08:35 -0400

    feat: allow server.getContractData's contractId to be a Contract Address (#97)
    * feat: allow server.getContractData's contractId to be a Contract Address
    
    * feat: use stellar-base to parse contract_id string

--- test/unit/server/get_contract_data_test.js
+++ test/unit/server/get_contract_data_test.js
@@ -1,5 +1,6 @@
 const MockAdapter = require("axios-mock-adapter");
 let xdr = SorobanClient.xdr;
+const { Address } = require("stellar-base");
 
 describe("Server#getContractData", function () {
   beforeEach(function () {
@@ -14,6 +15,8 @@ describe("Server#getContractData", function () {
 
   let address =
     "0000000000000000000000000000000000000000000000000000000000000001";
+  let nonHexAddress =
+    "CCJZ5DGASBWQXR5MPFCJXMBI333XE5U3FSJTNQU7RIKE3P5GN2K2WYD5";
   let key = SorobanClient.xdr.ScVal.scvVec([
     SorobanClient.xdr.ScVal.scvSymbol("Admin"),
   ]);
@@ -84,6 +87,40 @@ describe("Server#getContractData", function () {
 
     this.server
       .getContractData(address, key)
+      .then(function (_response) {
+        done(new Error("Expected error"));
+      })
+      .catch(function (err) {
+        done(
+          err.code == 404
+            ? null
+            : new Error("Expected error code 404, got: " + err.code)
+        );
+      });
+  });
+
+  it("key not found Non-hex address", function (done) {
+    this.axiosMock
+      .expects("post")
+      .withArgs(serverUrl, {
+        jsonrpc: "2.0",
+        id: 1,
+        method: "getLedgerEntries",
+        params: [
+          [
+            xdr.LedgerKey.contractData(
+              new xdr.LedgerKeyContractData({
+                contractId: Address.fromString(nonHexAddress).toBuffer(),
+                key,
+              })
+            ).toXDR("base64"),
+          ],
+        ],
+      })
+      .returns(Promise.resolve({ data: { result: { entries: [] } } }));
+
+    this.server
+      .getContractData(nonHexAddress, key)
       .then(function (_response) {
         done(new Error("Expected error"));
       })
