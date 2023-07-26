10b97c2d6e2aa52af10e090a255d82ffc4965cf7
George Kudrayvtsev <george@stellar.org>
2023-07-26 15:46:15 -0700

    Preserve existing auth entries after simulation if present

--- src/transaction.ts
+++ src/transaction.ts
@@ -62,15 +62,22 @@ export function assembleTransaction(
   switch (raw.operations[0].type) {
     case "invokeHostFunction":
       const invokeOp: Operation.InvokeHostFunction = raw.operations[0];
+      const existingAuth = invokeOp.auth ?? [];
       txnBuilder.addOperation(
         Operation.invokeHostFunction({
           source: invokeOp.source,
           func: invokeOp.func,
-          // override auth from the simulation: any existing `invokeOp.auth`
-          // should be a subset of what simulation gives us
-          auth: simulation.results[0].auth?.map(
-            (a) => xdr.SorobanAuthorizationEntry.fromXDR(a, "base64")
-          ) ?? []
+          // if auth entries are already present, we consider this "advanced
+          // usage" and disregard ALL auth entries from the simulation
+          //
+          // the intuition is "if auth exists, this tx has probably been
+          // simulated before"
+          auth:
+            existingAuth.length > 0
+              ? existingAuth
+              : simulation.results[0].auth?.map((a) =>
+                  xdr.SorobanAuthorizationEntry.fromXDR(a, "base64")
+                ) ?? [],
         })
       );
       break;
