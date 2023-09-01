68b1065e8fb6b2fb83cb046515f93ba3ac1cd19d
George Kudrayvtsev <george@stellar.org>
2023-09-01 16:59:43 -0700

    Drop misc. axios stuff nobody cares about

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 /* tslint:disable:no-var-requires */
-const version = require("../package.json").version;
 
 // Expose all types
 export * from "./soroban_rpc";
@@ -7,16 +6,10 @@ export { ContractSpec } from "./contract_spec";
 
 // stellar-sdk classes to expose
 export { Server } from "./server";
-export {
-  default as AxiosClient,
-  SERVER_TIME_MAP,
-  getCurrentServerTime,
-} from "./axios";
-export * from "./transaction";
+export { AxiosClient, version } from "./axios";
+export { assembleTransaction } from "./transaction";
 
 // expose classes and functions from stellar-base
 export * from "stellar-base";
-
-export { version };
 
 export default module.exports;
